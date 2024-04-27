7c7e63860dd8dbb94b8f09fb164c69a15bffc608
Evgeny Starikov <genestar@yandex.ru>
2024-04-27 22:22:44 +0300

    main.java modified

--- src/main.java
+++ src/main.java
@@ -1,2 +1,19 @@
 public class main {
+    public static void main(String[] args) {
+
+        int stoimost = 29007;
+
+        int mil_eqwiwalent = 20;
+
+        int bonus = stoimost / mil_eqwiwalent;
+
+        System.out.println("Вами потрачено, руб.: " +stoimost);
+        System.out.println("Количество накопленных миль: " + bonus);
+
+        // Вопрос к преподавателю: как написать (синтаксис),
+        // чтобы "руб." и "миль" отображались
+        // после числа в той же строке?
+        // то есть: "Вами потрачено 29007 руб."
+
+           }
 }
